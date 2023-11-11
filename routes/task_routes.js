ed70d7433d6e3acf6951ae6504bad41a1ed4a825
DinukaKaveen <94813111+DinukaKaveen@users.noreply.github.com>
2023-11-11 15:40:39 +0530

    create view tasks

--- routes/task_routes.js
+++ routes/task_routes.js
@@ -10,17 +10,34 @@ router.post("/new_task", (req, res) => {
     .save()
     .then((result) => {
       console.log(result);
-      return res.status(400).json({
+      return res.status(200).json({
         success: true,
         message: "Task Creating Success",
       });
     })
     .catch((err) => {
       console.error(err);
-      return res.status(200).json({
+      return res.status(400).json({
         success: false,
         message: "Task Creating Fail",
       });
+    });
+});
+
+router.get("/view_tasks", (req, res) => {
+  Task.find()
+    .then((result) => {
+      return res.status(200).json({
+        success: true,
+        tasks: result,
+      });
+    })
+    .catch((err) => {
+      console.error(err);
+      return res.status(400).json({
+        success: false,
+        message: "Tasks Loading Fail"
+      })
     });
 });
 
