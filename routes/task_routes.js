d1bfab949bf1ddddeb8a4a6a34df7aa514e59d62
DinukaKaveen <94813111+DinukaKaveen@users.noreply.github.com>
2023-11-11 14:50:30 +0530

    Update task_routes.js

--- routes/task_routes.js
+++ routes/task_routes.js
@@ -4,21 +4,24 @@ const Task = require("../models/tasks");
 const router = express.Router();
 
 router.post("/new_task", (req, res) => {
-  const newTask = new Task(req.body);
+  let newTask = new Task(req.body);
 
-  newTask.save((err) => {
-    if (err) {
+  newTask
+    .save()
+    .then((result) => {
+      console.log(result);
       return res.status(400).json({
         success: false,
-        message: "Task Creating Fail",
-      });
-    } else {
-      return res.status(200).json({
-        success: true,
         message: "Task Creating Success",
       });
-    }
-  });
+    })
+    .catch((err) => {
+      console.error(err);
+      return res.status(200).json({
+        success: true,
+        message: "Task Creating Fail",
+      });
+    });
 });
 
 module.exports = router;
