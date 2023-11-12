72716be5744fd86a2830119abf83d1899ac9b5ad
DinukaKaveen <94813111+DinukaKaveen@users.noreply.github.com>
2023-11-12 11:16:29 +0530

    use cors

--- server.js
+++ server.js
@@ -2,18 +2,20 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser"); //body-parser is used to convert json format into javascript object.
 const taskRoutes = require("./routes/task_routes");
+const cors = require("cors");
 
 const app = express();
 
 //app MiddleWare
 app.use(bodyParser.json());
+app.use(cors());
 
 //route MiddleWare
 app.use(taskRoutes);
 
 const PORT = 8000;
-const DB_URL = "mongodb+srv://dinuka:1234@cluster0.ozczact.mongodb.net/?retryWrites=true&w=majority";
-
+const DB_URL =
+  "mongodb+srv://dinuka:1234@cluster0.ozczact.mongodb.net/?retryWrites=true&w=majority";
 
 // coonect app with MongoDB
 mongoose
@@ -24,7 +26,6 @@ mongoose
   .catch((err) => {
     console.log("DB Connection Error: ", err);
   });
-
 
 // listen express app on port 8000
 app.listen(PORT, () => {
