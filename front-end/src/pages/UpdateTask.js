712d38581d838e8c1de8a8a237f7482d81bf53d5
DinukaKaveen <94813111+DinukaKaveen@users.noreply.github.com>
2023-11-13 11:23:31 +0530

    Update UpdateTask.js

--- front-end/src/pages/UpdateTask.js
+++ front-end/src/pages/UpdateTask.js
@@ -166,6 +166,7 @@ function UpdateTask() {
                   class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                   placeholder=" "
                   required
+                  disabled
                 />
               </div>
 
@@ -192,6 +193,7 @@ function UpdateTask() {
                   class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                   placeholder=" "
                   required
+                  disabled
                 />
               </div>
             </div>
@@ -245,7 +247,7 @@ function UpdateTask() {
               type="submit"
               className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             >
-              Submit
+              Update Task
             </button>
           </form>
         </div>
