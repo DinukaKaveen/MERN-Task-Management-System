bc5c96f8edcde771ec57b0335520d36f65a1c5e5
DinukaKaveen <94813111+DinukaKaveen@users.noreply.github.com>
2023-11-13 18:23:56 +0530

    add method to userShema for generates a token for user

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
 
 const userSchema = new mongoose.Schema({
 
@@ -23,5 +24,13 @@ const userSchema = new mongoose.Schema({
         default: false
     },
 });
+
+// add method to userShema for generates a JSON Web Token(JWT) for user
+userSchema.methods.generateAuthToken = function () {
+    const token = jwt.sign({_id: this._id}, process.env.JWTPRIVATEKEY, {
+        expiresIn: "7d",
+    });
+    return token;
+}
 
 module.exports = mongoose.model('users', userSchema);
