40e9dffd685505e90c1e16c518a20028e3a4a707
DinukaKaveen <94813111+DinukaKaveen@users.noreply.github.com>
2023-11-13 11:19:56 +0530

    Update UpdateTask.js

--- front-end/src/pages/UpdateTask.js
+++ front-end/src/pages/UpdateTask.js
@@ -152,16 +152,23 @@ function UpdateTask() {
                   onChange={(e) => onInputChange(e)}
                   className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                   placeholder=" "
-                  required
                 />
                 <label
                   htmlFor="addedDate"
                   className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
                 >
-                  Added Date
+                  Select Added Date
                 </label>
-                {taskDetails.addedDate}
+                <input
+                  type="text"
+                  name="addedDate"
+                  value={taskDetails.addedDate}
+                  class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                  placeholder=" "
+                  required
+                />
               </div>
+
               <div className="relative z-0 w-full mb-6 group">
                 <input
                   type="date"
@@ -171,15 +178,21 @@ function UpdateTask() {
                   onChange={(e) => onInputChange(e)}
                   className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                   placeholder=" "
-                  required
                 />
                 <label
                   htmlFor="dueDate"
                   className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
                 >
-                  Due Date
+                  Select Due Date
                 </label>
-                {taskDetails.dueDate}
+                <input
+                  type="text"
+                  name="addedDate"
+                  value={taskDetails.dueDate}
+                  class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                  placeholder=" "
+                  required
+                />
               </div>
             </div>
             <br />
