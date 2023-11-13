5b7bb6759f028a780cc85e66d8f2dbae3cc2a6b4
DinukaKaveen <94813111+DinukaKaveen@users.noreply.github.com>
2023-11-13 11:36:51 +0530

    Update UpdateTask.js

--- front-end/src/pages/UpdateTask.js
+++ front-end/src/pages/UpdateTask.js
@@ -163,7 +163,7 @@ function UpdateTask() {
                   type="text"
                   name="addedDate"
                   value={taskDetails.addedDate}
-                  class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                  class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-white appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                   placeholder=" "
                   required
                   disabled
@@ -190,7 +190,7 @@ function UpdateTask() {
                   type="text"
                   name="addedDate"
                   value={taskDetails.dueDate}
-                  class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                  class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-white appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                   placeholder=" "
                   required
                   disabled
