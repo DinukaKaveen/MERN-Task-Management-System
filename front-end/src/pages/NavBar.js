c46477714eb8f8b27d7a75996298b860b4f60f53
DinukaKaveen <94813111+DinukaKaveen@users.noreply.github.com>
2023-11-14 19:43:58 +0530

    Update NavBar.js

--- front-end/src/pages/NavBar.js
+++ front-end/src/pages/NavBar.js
@@ -112,7 +112,7 @@ function NavBar() {
           <ul className="space-y-2 font-medium">
             <li>
               <Link
-                to={"/"}
+                to={"/tasks"}
                 className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
               >
                 <svg
