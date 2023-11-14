bf99b2a016dc083ccead57ecd379349a25ce1a6a
DinukaKaveen <94813111+DinukaKaveen@users.noreply.github.com>
2023-11-14 19:43:52 +0530

    Update App.js

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,18 +1,22 @@
 import './App.css';
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import NavBar from './pages/NavBar';
 import Home from './pages/Home';
 import CreateTask from './pages/CreateTask';
 import UpdateTask from './pages/UpdateTask';
+import UserLogin from './pages/UserLogin';
+import UserRegister from './pages/UserRegister';
+import UserVerify from './pages/UserVerify';
 
 function App() {
   return (
       <BrowserRouter>
       <div>
-        <NavBar/>
         <Routes>
-          <Route path='/' exact element={<Home/>}></Route>
+          <Route path='/tasks' exact element={<Home/>}></Route>
+          <Route path='/' exact element={<UserLogin/>}></Route>
+          <Route path='/register' exact element={<UserRegister/>}></Route>
+          <Route path='/:id/verify/:token' exact element={<UserVerify/>}></Route>
           <Route path='/create_task' exact element={<CreateTask/>}></Route>
           <Route path='/update_task/:id' exact element={<UpdateTask/>}></Route>
         </Routes>
